61cb24eae51239e3cd03c563419634163ccc04ae
Hartawan Bahari M <46642970+darkun7@users.noreply.github.com>
2019-10-01 22:31:28 +0700

    Create Steepest Ascent HC (pseudo code).bat
    This code just a pseudo code, mainly I used combination of Java and PHP while write it. So, I need community help to fix it.

---
+++ Steepest Ascent HC (pseudo code).bat
@@ -0,0 +1,32 @@
+/*
+* Author @darkun7 (Hartawan Bahari M)
+* This code just Pseudo Code
+*/
+masalah;
+saat_ini instanceof Node;
+berikut instanceof Node;
+
+hillClimb(masalah){
+	step = [];
+	return [solusi, step];
+}
+
+fungsiMinimum(masalah suksesor){
+	berikut = max(suksesor);
+	step = push(masalah);
+	while(true){
+		if(berikut<suksesor){
+			saat_ini = berikut;
+			break
+		}
+	}
+	return berikut
+}
+
+main{
+	search = hillClimb(fungsiMinimum(masalah));
+	echo('Tahapan dari solusi: ');
+	search->step;
+	echo('Nilai solusi: ');
+	search->solusi;
+}
